3056fe9688424e85f41d5cc5e2b43f6892e9c1b9
Daniel <talonskullcustoms@gmail.com>
2022-07-28 21:28:14 -0400

    create test offsprings for concept

---
+++ Control.gd
@@ -0,0 +1,48 @@
+extends Control
+
+
+#import random
+var random = RandomNumberGenerator.new()
+
+#test family
+var family = {
+	"1A":{"id":"1A","skin":1,"eye":1,"sex":"male","father":null,"mother":null,"gen":1},
+	"2B":{"id":"2B","skin":2,"eye":2,"sex":"female","father":null,"mother":null,"gen":1}
+}
+
+func genTestId():
+	randomize()
+	var newId = str(random.randi_range(1,9999))
+	while(newId in family.keys()):
+		newId = str(random.randi_range(1,9999))
+	return newId
+	
+func createOffspring(father,mother):
+	print("F,M",father,mother)
+	var newborn = {}
+	newborn["id"] = genTestId()
+	newborn["sex"] = ["male","female"][random.randi_range(0,1)]
+	newborn["father"] = father["id"]
+	newborn["mother"] = mother["id"]
+	var fields = ["skin","eye"]
+	for i in fields:
+		newborn[i] = [father[i],mother[i]][random.randi_range(0,1)]
+	
+	family[newborn["id"]] = newborn
+	return newborn
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Button_pressed():
+	createOffspring(family["1A"],family["2B"])
+	print(family)
